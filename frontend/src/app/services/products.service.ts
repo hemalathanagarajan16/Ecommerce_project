3f09837514207c1872656995c2889564f315b2df
Hemalatha <hemalatha151630@gmail.com>
2025-10-11 22:28:16 +0530

    Update Angular environment files with production API URL

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService { 
-   private apiUrl = 'http://localhost:5000/api/products'; // Node.js backend URL
+  private apiUrl = `${environment.apiUrl}/products`;
   
   constructor(private http: HttpClient) { }
   getProducts(): Observable<Product[]> {
