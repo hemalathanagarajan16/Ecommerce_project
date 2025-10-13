b90dfed82a366b9f65201c8b7ff7428fc6363223
hemalathanagarajan16 <hemalatha151630@gmail.com>
2025-10-13 12:07:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:4200", // for local dev
   "https://hebaecommerce.vercel.app" // Deployed frontend on Vercel
+  "https://hebaecommerce-hemalathas-projects-4e5989bd.vercel.app" 
 ];
 
 app.use(
